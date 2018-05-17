d3307f0a93a224e7f4e856734e6c9852df813ddc
Parker Illig <parker.illig@colorado.edu>
2018-05-16 21:39:21 -0600

    doing a subtree update

--- exchange_react/src/App.js
+++ exchange_react/src/App.js
@@ -10,6 +10,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to the exchange!</h1>
         </header>
+	    <p>
+		COMING SOON
+	    </p>
       </div>
     );
   }
